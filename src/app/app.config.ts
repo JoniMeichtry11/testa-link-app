66ff63f7c416d3e38a575435093ada791974efec
JoniMeichtry11 <joni.meichtry@gmail.com>
2025-08-28 00:20:37 -0300

    feat: deploy in firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "testa-hermanos-linktre", appId: "1:193071275948:web:6172839bf77bae555f2409", storageBucket: "testa-hermanos-linktre.firebasestorage.app", apiKey: "AIzaSyDI3hv-zvNcAEBi4tC8IrgCMQlTYHifqFM", authDomain: "testa-hermanos-linktre.firebaseapp.com", messagingSenderId: "193071275948", measurementId: "G-2E41HKWZRP" })), provideStorage(() => getStorage())
   ]
 };
